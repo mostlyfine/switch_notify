17cda15f54936a79621b71fbf801b722f0bf5bf6
Seiji Sawayanagi <sawayanagi@1101.com>
2017-06-30 20:47:48 +0900

    Add switch page checker

---
+++ switch.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var client = require('cheerio-httpcli');
+var ifttt = require('./ifttt');
+
+module.exports.check = (event, context, callback) => {
+  client.fetch('https://store.nintendo.co.jp/customize.html', function(err, $, res) {
+    if (err) {
+      callback(err);
+    } else {
+      $('.items').each(function(i) {
+        var stock = $(this).text().split('/');
+        if (stock[0] == 'HAC_S_KAYAA' && stock[1] != '-') {
+          ifttt.notify(proces.env.MESSAGE);
+          callback(null, stock[1]);
+        }
+      });
+    }
+  });
+};
